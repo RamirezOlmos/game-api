2745744ba1eb3303b72d34f9730c16039bd6f2b9
RamirezOlmos <nyy_715074@yahoo.com>
2023-01-31 14:48:50 -0600

    changes in res.header

--- src/app.js
+++ src/app.js
@@ -19,7 +19,7 @@ server.use(morgan('dev'));
 server.use((req, res, next) => {
   res.header('Access-Control-Allow-Origin', 'https://game-api-production.up.railway.app'); // update to match the domain you will make the request from
   res.header('Access-Control-Allow-Credentials', 'true');
-  res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
+  res.header('Access-Control-Allow-Headers', 'Origin');
   res.header('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, DELETE');
   next();
 });
